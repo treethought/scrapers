b8c7f1919d1dbe5a0be995572855221108bc5610
treethought <cpsweene@gmail.com>
2016-10-17 21:18:06 -0700

    added reddit

---
+++ reddit_media/config.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+MEDIA = os.path.join(ROOT, 'media')
+PICS = os.path.join(MEDIA, 'pics')
+MUSIC = os.path.join(MEDIA, 'music')
+VIDEO = os.path.join(MEDIA, 'videos')
